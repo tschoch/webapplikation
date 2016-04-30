0496411518f414d2346a9da4a45766f595e9919f
bmosberger <benjamin.mosberger@ntb.ch>
2016-04-30 09:16:08 +0200

    file_upload
    upload mit file_type Kontrolle & limitierter imagesize

--- Website/send_post.php
+++ Website/send_post.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start(); 
 require '../phplogin/dbconfig.php';
@@ -82,43 +83,106 @@ $query_place= "UPDATE Users SET City='$place' WHERE Fuid='$fuid'";
 mysql_query($query_place);    
 }
 
-//fileupload
-/*$uploaddir = 'uploads/';
-$uploadfile = $uploaddir . basename($_FILES['picture']['name']);
-move_uploaded_file($_FILES['picture']['tmp_name'], $uploadfile); */
+
+
+
 $count=1;
 
-if($count > 3):
-foreach ($_FILES["pictures"]["error"] as $key => $error) {
-    $filename = $_FILES["pictures"]["tmp_name"][$key];
-    $ext = pathinfo($filename, PATHINFO_EXTENSION);
-    
-    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"):
-    if ($error == UPLOAD_ERR_OK) {
-        $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
-        $name = $_FILES["pictures"]["name"][$key];
-        move_uploaded_file($tmp_name, "uploads/$fuid_$count");
+if($count < 4):
+foreach ($_FILES["pictures"]["error"] as $key => $error) { 
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["pictures"]["name"][$key]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["pictures"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
     }
-    $count++;
-    endif;
 }
-else:
-$count=1;
-foreach ($_FILES["pictures"]["error"] as $key => $error) {
-    //$check = getimagesize($_FILES["pictures"]["tmp_name"][$key]);
-    $filename = $_FILES["pictures"]["tmp_name"][$key];
-    $ext = pathinfo($filename, PATHINFO_EXTENSION);
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
+    $name = $_FILES["pictures"]["name"][$key];
     
-    //if(($check !== false) && (in_array($ext,$allowed)):
-    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"):
-    if ($error == UPLOAD_ERR_OK) {
-        $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
-        $name = $_FILES["pictures"]["name"][$key];
-        move_uploaded_file($tmp_name, "uploads/$fuid"."_"."$count");
+    if (move_uploaded_file($tmp_name, "uploads/$fuid"."_"."$count")) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
     }
+}
     $count++;
-    endif;
-};
+}
+        
+ else:   
+    $count=1;
+foreach ($_FILES["pictures"]["error"] as $key => $error) { 
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["pictures"]["name"][$key]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["pictures"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
+    $name = $_FILES["pictures"]["name"][$key];
+    
+    if (move_uploaded_file($tmp_name, "uploads/$fuid"."_"."$count")) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+    $count++;
+}
+    
+    
+
 endif;
 
              
