44736bab69f95698c6f59749284b4b586c979f05
tschoch <tschoch@users.noreply.github.com>
2016-05-26 09:47:05 +0200

    Insert new Users

--- sql/insert_skript_user.sql
+++ sql/insert_skript_user.sql
@@ -1,1 +1,9 @@
-INSERT INTO `13_1`.`Users` (`UID`, `Fuid`, `Ffname`, `Femail`, `Offer_1`, `Offer_2`, `Offer_3`, `Street`, `Nr`, `PLZ`, `City`, `lat`, `lng`) VALUES (NULL, '123456', 'Hans Noetig', 'hans.noetig@carparts.ch', 'Motor Reparaturen', 'Elektrik', 'Getriebe Reparaturen', 'Pulvermühlestrasse', '57', '7000', 'Chur', '', ''), (NULL, '987654', 'Johnny Lässig', 'johnny_lässig@carparts.ch', 'Bremsen Reparaturen', 'Pneu Reparaturen und Wechsel', 'Lenkgeometrie', 'Werdenbergstrasse', '4', '9470', 'Buchs', '', '');
+INSERT INTO `Users` (`UID`, `Fuid`, `Ffname`, `Femail`, `Offer_1`, `Offer_2`, `Offer_3`, `Street`, `Nr`, `PLZ`, `City`, `lat`, `lng`) VALUES
+(1, '848871701902987', 'Benjamin Mosberger', 'benjamin.mosberger@hotmail.com', 'Bremsen', 'pneu', 'reifen', 'Wiedenstrasse ', 15, 9323, 'Steinach', 47.5029, 9.43483),
+(2, '120445915025924', 'Tobias Schoch', 'schoch.tobias@gmail.com', 'reifen', 'fenster', 'bremsen', 'Sonnmattstrasse', 3, 9320, 'Arbon', 47.517, 9.41714),
+(6, '1078105118918571', 'Niklaus Schoch', 'nikboykick@gmail.com', 'Fenster ', 'Reifen', 'Folieren', 'Eichenstrasse ', 46, 9320, 'Arbon', 47.5129, 9.42437),
+(7, '1099436336784538', 'Remo Stutz', 'stutz_remo@hotmail.com', 'reifen', 'bremsen', '', 'FÃ¶hrenstrasse', 4, 9320, 'Arbon', 47.5094, 9.41877),
+(8, '10209593952915043', 'Daniel Zeba', 'dani_zeba@hotmail.com', 'Reifen', 'Licht', 'radio', 'Im LÃ¶ffelchratte', 13, 9320, 'Stachen', 47.5065, 9.40761),
+(9, '10209530983819533', 'Tiziano Sguazzato', 'b_i_g_ti@msn.com', 'Licht', 'Reifen', 'Auto', 'Wiesentalstrasse', 8, 9320, 'Arbon', 47.5086, 9.42672),
+(10, '1088411651220239', 'Manuel Zeba', 'man-z@hotmail.com', 'Tuning', 'Reifen', 'Motorrevision', 'Waldeggstrasse ', 1, 9320, 'Arbon', 47.5092, 9.41799),
+(11, '10207963643240431', 'Fabian ThÃ¼rlemann', 'fabian.th@bluewin.ch', 'reifen', 'motor', 'felgen', 'Hintergasse ', 4, 9320, 'Arbon', 47.5165, 9.43501);
