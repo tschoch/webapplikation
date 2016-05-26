6c9d30a7ad8a3c02af92d23d89a4d6adb72e2ee3
bmosberger <benjamin.mosberger@ntb.ch>
2016-05-26 17:39:08 +0200

    session_destroy

--- phplogin/logout.php
+++ phplogin/logout.php
@@ -1,8 +1,5 @@
 <?php 
 session_start();
-session_unset();
-    $_SESSION['FBID'] = NULL;
-    $_SESSION['FULLNAME'] = NULL;
-    $_SESSION['EMAIL'] =  NULL;
+session_destroy();
 header("Location: ../Website/search_list.php");        
 ?>
